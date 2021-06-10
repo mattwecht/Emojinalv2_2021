27b989b61110de63d3fc23ace7c4c8efab662799
Matt Wecht <mattwecht@gmail.com>
2021-06-10 16:48:15 -0400

    Initial Commit

---
+++ Emojinalv2_2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emojinalv2_2021
+//
+//  Created by Matthew Wecht on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
