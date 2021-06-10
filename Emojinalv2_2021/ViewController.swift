562f87b40bfaf2f6ecb0a58f8b7dc799f069d160
Matt Wecht <mattwecht@gmail.com>
2021-06-10 17:40:10 -0400

    Iteration 1

--- Emojinalv2_2021/ViewController.swift
+++ Emojinalv2_2021/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController()
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+        
+      // user clicks button
+        // alert message shows up
+        // alert prints message
+        // user can get rid of the alert
+        
 
-}
+    }
+
+}//end of the class. No code below me
 
